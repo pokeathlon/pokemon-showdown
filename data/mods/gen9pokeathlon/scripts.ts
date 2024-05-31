a256267bc7ed0dadc66fb5adb4212380664f81d4
Dunscy <0acrespo0@gmail.com>
2024-05-31 17:12:58 +0100

    Update scripts.ts

--- data/mods/gen9pokeathlon/scripts.ts
+++ data/mods/gen9pokeathlon/scripts.ts
@@ -91,41 +91,57 @@ const PoADex: {[k: string]: number} = {
 	"jovianshk": 6014,
 	"lunachi": 6015,
 	"ockthane": 6016,
+	"porygon2rhinian": 6017,
+	"porygonzrhinian": 6018,
 	"incineroarolulian": 727,
 	"raikousupra": 243,
 	"enteisupra": 244,
 	"suicunesupra": 245,
 	"heatransupra": 485,
-	"mosster": 6017,
-	"barrimander": 6018,
-	"staruhz": 6019,
-	"wrighvern": 6020,
-	"meditao": 6021,
+	"mosster": 6019,
+	"barrimander": 6020,
+	"staruhz": 6021,
+	"wrighvern": 6022,
+	"meditao": 6023,
 	"electrodemega": 101,
 	"florgesmega": 671,
-	"pandiz": 6022,
-	"bewitwing": 6023,
-	"eidolburgh": 6024,
+	"pandiz": 6024,
+	"bewitwing": 6025,
+	"eidolburgh": 6026,
 	"snorlaxfrost": 143,
 	"snorlaxfrostmega": 143,
 	"heracrosssubarctic": 214,
-	"sirentom": 6025,
-	"braskeptic": 6026,
-	"maggony": 6027,
-	"sweepdol": 6028,
-	"paldemaria": 6029,
-	"monetoad": 6030,
+	"sirentom": 6027,
+	"braskeptic": 6028,
+	"maggony": 6029,
+	"sweepdol": 6030,
+	"paldemaria": 6031,
+	"monetoad": 6032,
 	"drifloonkitakami": 425,
-	"drifbozu": 6031,
-	"nestitan": 6032,
-	"loxicant": 6033,
-	"manacra": 6034,
-	"manacraplated": 6034,
-	"feidan": 6035,
-	"porygon2rhinian": 6036,
-	"porygonzrhinian": 6037,
-	"niandertroll": 6038,
-	"scorchingkiln": 6039,
+	"drifbozu": 6033,
+	"voliable": 6034,
+	"nestitan": 6035,
+	"loxicant": 6036,
+	"manacra": 6037,
+	"manacraplated": 6037,
+	"feidan": 6038,
+	"niandertroll": 6039,
+	"scorchingkiln": 6040,
+	"berserkergene": 6041,
+	"scalyterror": 6042,
+	"pestri": 6043,
+	"sinistersickle": 6044,
+	"larvitardelta": 246,
+	"pupitardelta": 247,
+	"tyranitardelta": 248,
+	"blazikendeltamega": 257,
+	"sceptiledeltamega": 254,
+	"felapstan": 6045,
+	"tinkastab": 6046,
+	"tinkaslice": 6047,
+	"tinkashank": 6048,
+	"grandmirage": 6049,
+
 };
 
 export const Scripts: ModdedBattleScriptsData = {
@@ -146,5 +162,107 @@ export const Scripts: ModdedBattleScriptsData = {
 				delete this.data.Moves[i].isNonstandard;
 			}
 		}
+	},
+	actions: {
+		switchIn(pokemon: Pokemon, pos: number, sourceEffect: Effect | null = null, isDrag?: boolean) {
+			if (!pokemon || pokemon.isActive) {
+				this.battle.hint("A switch failed because the Pokémon trying to switch in is already in.");
+				return false;
+			}
+	
+			const side = pokemon.side;
+			if (pos >= side.active.length) {
+				throw new Error(`Invalid switch position ${pos} / ${side.active.length}`);
+			}
+			const oldActive = side.active[pos];
+			const unfaintedActive = oldActive?.hp ? oldActive : null;
+			if (unfaintedActive) {
+				oldActive.beingCalledBack = true;
+				let switchCopyFlag: 'copyvolatile' | 'shedtail' | boolean = false;
+				if (sourceEffect && typeof (sourceEffect as Move).selfSwitch === 'string') {
+					switchCopyFlag = (sourceEffect as Move).selfSwitch!;
+				}
+				if (!oldActive.skipBeforeSwitchOutEventFlag && !isDrag) {
+					this.battle.runEvent('BeforeSwitchOut', oldActive);
+					if (this.battle.gen >= 5) {
+						this.battle.eachEvent('Update');
+					}
+				}
+				oldActive.skipBeforeSwitchOutEventFlag = false;
+				if (oldActive.getVolatile('preventswitch')) {
+					oldActive.removeVolatile('preventswitch');
+					return false;
+				}
+				if (!this.battle.runEvent('SwitchOut', oldActive)) {
+					// Warning: DO NOT interrupt a switch-out if you just want to trap a pokemon.
+					// To trap a pokemon and prevent it from switching out, (e.g. Mean Look, Magnet Pull)
+					// use the 'trapped' flag instead.
+	
+					// Note: Nothing in the real games can interrupt a switch-out (except Pursuit KOing,
+					// which is handled elsewhere); this is just for custom formats.
+					return false;
+				}
+				if (!oldActive.hp) {
+					// a pokemon fainted from Pursuit before it could switch
+					return 'pursuitfaint';
+				}
+	
+				// will definitely switch out at this point
+	
+				oldActive.illusion = null;
+				this.battle.singleEvent('End', oldActive.getAbility(), oldActive.abilityState, oldActive);
+	
+				// if a pokemon is forced out by Whirlwind/etc or Eject Button/Pack, it can't use its chosen move
+				this.battle.queue.cancelAction(oldActive);
+	
+				let newMove = null;
+				if (this.battle.gen === 4 && sourceEffect) {
+					newMove = oldActive.lastMove;
+				}
+				if (switchCopyFlag) {
+					pokemon.copyVolatileFrom(oldActive, switchCopyFlag);
+				}
+				if (newMove) pokemon.lastMove = newMove;
+				oldActive.clearVolatile();
+			}
+			if (oldActive) {
+				oldActive.isActive = false;
+				oldActive.isStarted = false;
+				oldActive.usedItemThisTurn = false;
+				oldActive.statsRaisedThisTurn = false;
+				oldActive.statsLoweredThisTurn = false;
+				oldActive.position = pokemon.position;
+				pokemon.position = pos;
+				side.pokemon[pokemon.position] = pokemon;
+				side.pokemon[oldActive.position] = oldActive;
+			}
+			pokemon.isActive = true;
+			side.active[pos] = pokemon;
+			pokemon.activeTurns = 0;
+			pokemon.activeMoveActions = 0;
+			for (const moveSlot of pokemon.moveSlots) {
+				moveSlot.used = false;
+			}
+			this.battle.runEvent('BeforeSwitchIn', pokemon);
+			if (sourceEffect) {
+				this.battle.add(isDrag ? 'drag' : 'switch', pokemon, pokemon.getDetails, '[from] ' + sourceEffect);
+			} else {
+				this.battle.add(isDrag ? 'drag' : 'switch', pokemon, pokemon.getDetails);
+			}
+			pokemon.abilityOrder = this.battle.abilityOrder++;
+			if (isDrag && this.battle.gen === 2) pokemon.draggedIn = this.battle.turn;
+			pokemon.previouslySwitchedIn++;
+	
+			if (isDrag && this.battle.gen >= 5) {
+				// runSwitch happens immediately so that Mold Breaker can make hazards bypass Clear Body and Levitate
+				this.battle.singleEvent('PreStart', pokemon.getAbility(), pokemon.abilityState, pokemon);
+				this.runSwitch(pokemon);
+			} else {
+				this.battle.queue.insertChoice({choice: 'runUnnerve', pokemon});
+				this.battle.queue.insertChoice({choice: 'runSwitch', pokemon});
+			}
+	
+			return true;
+		},
 	},
 };
