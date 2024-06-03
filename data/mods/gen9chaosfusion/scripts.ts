6cbc3dc57ce510e6b5b514f6aac45d3ba5b16223
vikrambarr <37145312+vikrambarr@users.noreply.github.com>
2024-06-03 12:43:25 +0200

    Update scripts.ts

--- data/mods/gen9chaosfusion/scripts.ts
+++ data/mods/gen9chaosfusion/scripts.ts
@@ -3,7 +3,8 @@ export const Scripts: ModdedBattleScriptsData = {
 	init() {
 		for (var i in this.data.Pokedex) {
 			if (this.data.Pokedex[i].types.includes('Nuclear')) {
-				this.data.FormatsData[i].natDexTier = "Uber";
+				if (this.data.FormatsData[i]) this.data.FormatsData[i].natDexTier = "Uber";
+				else this.data.FormatsData[i] = {tier: "Uber", natDexTier: "Uber"};
 			}
 		}
 	}
