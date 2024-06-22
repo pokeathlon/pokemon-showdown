2d0bd105dd1eb0cf6d4332a235814232b8dc2ba0
Dunscy <0acrespo0@gmail.com>
2024-06-22 10:32:18 +0100

    redundant

--- config/alt-formats.ts
+++ config/alt-formats.ts
@@ -381,7 +381,7 @@ export const Formats: FormatList = [
 		gameType: 'doubles',
 		ruleset: ['Standard NatDex', 'Tera Type Preview', 'Swagger Clause', 'OHKO Clause', 'Evasion Clause', 'Sleep Clause Mod', 'Max Level = 120', 'Default Level = 120', 'Overflow Stat Mod', `Picked Team Size = 4`],
 		banlist: ['move:hiddenpower', 'move:revivalblessing', 'move:shedtail', 'move:lastrespects', 'move:achillesheel', 'move:permafrost', 'move:livewire', 'move:nanorepair', 'move:jetstream', 
-				'ability:moody', 'ability:sandveil', 'ability:snowcloak', 'ability:illuminate', 'ability:winterjoy'],
+				'ability:moody', 'ability:illuminate', 'ability:winterjoy'],
 	},
 
 	{
