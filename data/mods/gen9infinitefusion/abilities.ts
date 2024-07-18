d21cec92eed71db96edc574ecc5c91a4f6d243a1
Dunscy <0acrespo0@gmail.com>
2024-07-18 19:05:58 +0100

    noguard mpaw maybe

--- data/mods/gen9infinitefusion/abilities.ts
+++ data/mods/gen9infinitefusion/abilities.ts
@@ -535,10 +535,10 @@ export const Abilities: {[k: string]: ModdedAbilityData} = {
 	noguard: {
 		inherit: true,
 		onAnyInvulnerability(target, source, move) {
-			if (move && !move.ohko && (source === this.effectState.target || target === this.effectState.target)) return 0;
+			if (move && !move.ohko && (!source.hasItem('mankeyspaw')) && (source === this.effectState.target || target === this.effectState.target)) return 0;
 		},
 		onAnyAccuracy(accuracy, target, source, move) {
-			if (move && !move.ohko && (source === this.effectState.target || target === this.effectState.target)) {
+			if (move && !move.ohko && (!source.hasItem('mankeyspaw')) && (source === this.effectState.target || target === this.effectState.target)) {
 				return true;
 			}
 			return accuracy;
