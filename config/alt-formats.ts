0ae958e908b9f447838b8b47f1f508d45402db63
Dunscy <0acrespo0@gmail.com>
2024-07-29 20:15:29 +0100

    whyyyyyyy

--- config/alt-formats.ts
+++ config/alt-formats.ts
@@ -506,7 +506,7 @@ export const Formats: FormatList = [
 		mod: 'gen9pokeathlon',
 		ruleset: ['Standard', 'Terastal Clause'],
 		banlist: ['Uber', 'AG', 'Arena Trap', 'Moody', 'Sand Veil', 'Shadow Tag', 'Snow Cloak', 'Baton Pass', 'Last Respects', 'Shed Tail', 'Multishot', 'Acupressure', 'Razor Fang', 'King\'s Rock',
-			'Focus Band', 'Quick Claw',],
+			'Focus Band'],
 	},
 	{
 		name: "[Gen 9] PoA Dex AG",
@@ -522,7 +522,7 @@ export const Formats: FormatList = [
 		mod: 'gen9pokeathlon',
 		ruleset: ['Standard', 'Terastal Clause'],
 		banlist: ['OU', 'UUBL', 'Uber', 'AG', 'Arena Trap', 'Moody', 'Sand Veil', 'Shadow Tag', 'Snow Cloak', 'Baton Pass', 'Last Respects', 
-			'Shed Tail', 'Multishot',  'Acupressure', 'Razor Fang', 'King\'s Rock', 'Focus Band', 'Quick Claw',],
+			'Shed Tail', 'Multishot',  'Acupressure', 'Razor Fang', 'King\'s Rock', 'Focus Band'],
 	},
 
 	{
@@ -537,7 +537,7 @@ export const Formats: FormatList = [
 		banlist: [
 			'ND Uber', 'ND AG', 'Arena Trap', 'Moody', 'Power Construct', 'Shadow Tag',
 			'Quick Claw', 'Assist', 'Baton Pass', 'Last Respects', 'Shed Tail',  'Acupressure', 'Razor Fang', 'King\'s Rock',
-			'Focus Band',
+			'Focus Band'
 		],
 	},
 	{
