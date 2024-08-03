28f12a500ba8c1e5cc79454fb3bfbec5b1f6058c
Dunscy <0acrespo0@gmail.com>
2024-08-03 14:46:09 +0100

    hmmm¿

--- data/mods/gen9pokeathlon/learnsets.ts
+++ data/mods/gen9pokeathlon/learnsets.ts
@@ -5221,12 +5221,7 @@ export const Learnsets: {[k: string]: ModdedLearnsetData} = {
 			stompingtantrum: ["9M"],
 		}
 	},
-	penumbralith: {inherit: true, learnset: {...baseLearnsets.solrock.learnset, 
-		...baseLearnsets.lunatone.learnset, 
-		lunarcannon: ["9M"],
-		expandingforce: ["9M"],
-		irondefense: ["9M"],
-		bodypress: ["9M"]}},
+	penumbralith: flattenLearnset(baseLearnsets.solrock,[],[baseLearnsets.lunatone, 'cometstrike']),
 
 	growlsome: {
 		learnset: {
