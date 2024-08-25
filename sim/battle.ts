ea76feefc1281e6d21407a815147fbd13874748b
vikrambarr <37145312+vikrambarr@users.noreply.github.com>
2024-08-25 17:53:16 +0100

    messed up merge

--- sim/battle.ts
+++ sim/battle.ts
@@ -2804,35 +2804,6 @@ export class Battle {
 	}
 
 	/**
-	 * Generally called at the beginning of a turn, to go through the
-	 * turn one action at a time.
-	 *
-	 * If there is a mid-turn decision (like U-Turn), this will return
-	 * and be called again later to resume the turn.
-	 */
-	turnLoop() {
-		this.add('');
-		this.add('t:', Math.floor(Date.now() / 1000));
-		if (this.requestState) this.requestState = '';
-
-		if (!this.midTurn) {
-			this.queue.insertChoice({choice: 'beforeTurn'});
-			this.queue.addChoice({choice: 'residual'});
-			this.midTurn = true;
-		}
-
-		let action;
-		while ((action = this.queue.shift())) {
-			this.runAction(action);
-			if (this.requestState || this.ended) return;
-		}
-
-		this.endTurn();
-		this.midTurn = false;
-		this.queue.clear();
-	}
-
-	/**
 	 * Takes a choice string passed from the client. Starts the next
 	 * turn if all required choices have been made.
 	 */
--- sim/team-validator.ts
+++ sim/team-validator.ts
@@ -513,12 +513,8 @@ export class TeamValidator {
 				tierSpecies = dex.species.get('Groudon-Primal');
 			} else if (item.id === 'blueorb' && species.id === 'kyogre') {
 				tierSpecies = dex.species.get('Kyogre-Primal');
-			} else if (item.id === 'crystalpiece' && species.id === 'giratina') {
-				tierSpecies = dex.species.get('Giratina-Primal');
-			} else if (item.id === 'crystalpiece' && species.id === 'arceus') {
-				tierSpecies = dex.species.get('Arceus-Primal');
-			} else if (item.id === 'crystalpiece' && species.id === 'regigigas') {
-				tierSpecies = dex.species.get('Regigigas-Primal');
+			} else if (item.id === 'crystalpiece' && ['giratina', 'arceus', 'regigigas'].includes(species.id)) {
+				tierSpecies = dex.species.get(species.id + 'primal');
 			} else if (canMegaEvo && species.id === 'rayquaza' && set.moves.map(toID).includes('dragonascent' as ID) &&
 					!ruleTable.has('megarayquazaclause')) {
 				tierSpecies = dex.species.get('Rayquaza-Mega');
