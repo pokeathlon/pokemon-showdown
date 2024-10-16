e3af69d29fc8b6743ba91fc399347d13e82a21a2
vikrambarr <37145312+vikrambarr@users.noreply.github.com>
2024-10-16 12:23:00 +0100

    forgot an import

--- data/mods/gen5xenoverse/abilities.ts
+++ data/mods/gen5xenoverse/abilities.ts
@@ -1,3 +1,4 @@
+const {Dex} = require('../../../sim/dex');
 export const Abilities: import('../../../sim/dex-abilities').ModdedAbilityDataTable = {
 	...Dex.deepClone(require('../gen9xenoverse/abilities').Abilities),
 };
