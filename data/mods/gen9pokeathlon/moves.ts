242f038a77298c28835584c08de0618ca99441ca
Dunscy <0acrespo0@gmail.com>
2024-11-01 20:28:16 +0000

    daf desc

--- data/mods/gen9pokeathlon/moves.ts
+++ data/mods/gen9pokeathlon/moves.ts
@@ -1273,6 +1273,8 @@ export const Moves: import('../../../sim/dex-moves').ModdedMoveDataTable = {
 		flags: {contact: 1, protect: 1, mirror: 1, metronome: 1},
 		selfSwitch: true,
 		secondary: null,
+		desc: "If this move is successful and the user has not fainted, the user switches out even if it is trapped and is replaced immediately by a selected party member. The user does not switch out if there are no unfainted party members, or if the target switched out using an Eject Button or through the effect of the Emergency Exit or Wimp Out Abilities.",
+		shortDesc: "User switches out after damaging the target.",
 		target: "normal",
 		type: "Grass",
 	},
