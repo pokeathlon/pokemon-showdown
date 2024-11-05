a6e2ab56480aade44595181809eb0c06f6ce46c0
vikrambarr <37145312+vikrambarr@users.noreply.github.com>
2024-11-05 12:52:22 +0000

    rewrite all eggroups

--- data/mods/gen9pokeathlon/pokedex.ts
+++ data/mods/gen9pokeathlon/pokedex.ts
@@ -120,7 +120,7 @@ for (var mon of additions) {
 		heightm: parseFloat(cur["heightm"]),
 		weightkg: parseFloat(cur["weightkg"]),
 		tags: ["Pokeathlon"],
-		eggGroups: ["Poa"],
+		eggGroups: ["Pokeathlon"],
 		tier: cur["tier"],
 		natDexTier: cur["natDexTier"],
 		doublesTier: cur["doublesTier"],
