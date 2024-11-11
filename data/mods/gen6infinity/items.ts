e450753f2403760f828bf810bc63cf7653625118
vikrambarr <37145312+vikrambarr@users.noreply.github.com>
2024-11-11 17:49:01 +0000

    legalize items

--- data/mods/gen6infinity/items.ts
+++ data/mods/gen6infinity/items.ts
@@ -1,2 +1,44 @@
 const {Dex} = require('../../../sim/dex');
-export const Items: import('../../../sim/dex-items').ModdedItemDataTable = Dex.deepClone(require('../gen9infinity/items').ModItems);
+export const Items: import('../../../sim/dex-items').ModdedItemDataTable = {
+	...Dex.deepClone(require('../gen9infinity/items').ModItems),
+	loadeddice: {
+		inherit: true,
+		gen: 6,
+		isNonstandard: null,
+	},
+	roomservice: {
+		inherit: true,
+		gen: 6,
+		isNonstandard: null,
+	},
+	covertcloak: {
+		inherit: true,
+		gen: 6,
+		isNonstandard: null,
+	},
+	punchingglove: {
+		inherit: true,
+		gen: 6,
+		isNonstandard: null,
+	},
+	throatspray: {
+		inherit: true,
+		gen: 6,
+		isNonstandard: null,
+	},
+	adrenalineorb: {
+		inherit: true,
+		gen: 6,
+		isNonstandard: null,
+	},
+	clearamulet: {
+		inherit: true,
+		gen: 6,
+		isNonstandard: null,
+	},
+	blunderpolicy: {
+		inherit: true,
+		gen: 6,
+		isNonstandard: null,
+	},
+};
