d4858e3912b4617283f091970ec8471218ebde13
Dunscy <0acrespo0@gmail.com>
2024-11-11 17:31:28 +0000

    Item Desc

--- data/mods/gen9infinity/items.ts
+++ data/mods/gen9infinity/items.ts
@@ -14,6 +14,7 @@ export const ModItems: import('../../../sim/dex-items').ModdedItemDataTable = {
 		itemUser: ["Terathwak"],
 		num: 0,
 		gen: 6,
+		shortDesc: "If held by a Terathwak, its critical hit ratio is raised by 2 stages.",
 	},
 };
 export const Items: import('../../../sim/dex-items').ModdedItemDataTable = Dex.deepClone(ModItems);
