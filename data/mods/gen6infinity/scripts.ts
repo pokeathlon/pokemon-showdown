d87e1fe94e27ba9596da76fdad34cb04d0b52dcd
vikrambarr <37145312+vikrambarr@users.noreply.github.com>
2024-11-13 02:34:23 +0000

    add reversal stone to gen 6

--- data/mods/gen6infinity/scripts.ts
+++ data/mods/gen6infinity/scripts.ts
@@ -1,3 +1,26 @@
 export const Scripts: ModdedBattleScriptsData = {
 	inherit: 'gen6',
+	init() {
+		for (let i in this.data.Pokedex) {
+			const mon = this.data.Pokedex[i];
+			if (mon.evos && mon.evos.length > 1) {
+				for (var receiver of [mon.name].concat(mon.evos)) {
+					if (!(this.toID(receiver) in this.data.Learnsets)) continue;
+					var learnset = this.data.Learnsets[this.toID(receiver)].learnset;
+					if (!learnset) learnset = {};
+					for (var evo of mon.evos) {
+						if (this.toID(evo) in this.data.Learnsets) {
+							var toadd = this.data.Learnsets[this.toID(evo)].learnset;
+							for (var move in toadd) {
+								for (var method of toadd[move as keyof typeof toadd]) {
+									if (!learnset[move as keyof typeof learnset]) learnset[move as keyof typeof learnset] = [];
+									if (!learnset[move as keyof typeof learnset].includes(method)) learnset[move as keyof typeof learnset].push(method);
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
 };
