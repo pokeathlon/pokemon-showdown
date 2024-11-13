14771df7fdbc2e375d5a5ac751ba098b91cfad4e
vikrambarr <37145312+vikrambarr@users.noreply.github.com>
2024-11-13 15:07:06 +0000

    thats weird

--- data/mods/gen9chaos/scripts.ts
+++ data/mods/gen9chaos/scripts.ts
@@ -25,15 +25,15 @@ export const Scripts: ModdedBattleScriptsData = {
 			if (mon.evos && mon.evos.length > 1) {
 				for (var receiver of [mon.name].concat(mon.evos)) {
 					if (!(this.toID(receiver) in this.data.Learnsets)) continue;
-					var learnset = this.data.Learnsets[this.toID(receiver)].learnset;
-					if (!learnset) learnset = {};
+					var locallearnset = Dex.mod('gen9chaos').learnsets.get(this.toID(receiver)).learnset;
+					if (!locallearnset) locallearnset = {};
 					for (var evo of mon.evos) {
 						if (this.toID(evo) in Dex.mod('gen6infinity').data.Learnsets) {
 							var tomerge = Dex.mod('gen6infinity').data.Learnsets[this.toID(evo)].learnset;
 							for (var move in tomerge) {
 								for (var lMethod of tomerge[move as keyof typeof tomerge]) {
-									if (!learnset[move as keyof typeof learnset]) learnset[move as keyof typeof learnset] = [];
-									if (!learnset[move as keyof typeof learnset].includes(lMethod)) learnset[move as keyof typeof learnset].push(lMethod);
+									if (!locallearnset[move as keyof typeof locallearnset]) locallearnset[move as keyof typeof locallearnset] = [];
+									if (!locallearnset[move as keyof typeof locallearnset].includes(lMethod)) locallearnset[move as keyof typeof locallearnset].push(lMethod);
 								}
 							}
 						}
