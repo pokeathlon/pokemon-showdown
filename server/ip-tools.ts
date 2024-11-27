38799dc1103b47baff278eb4668954bdfebd7da8
vikrambarr <37145312+vikrambarr@users.noreply.github.com>
2024-11-27 11:11:29 +0000

    even more dnsbls

--- server/ip-tools.ts
+++ server/ip-tools.ts
@@ -14,7 +14,7 @@
  * @license MIT
  */
 
-const BLOCKLISTS = ['sbl.spamhaus.org', 'rbl.efnetrbl.org', 'spam.spamrats.com', 'dyna.spamrats.com', 'all.s5h.net'];
+const BLOCKLISTS = ['sbl.spamhaus.org', 'rbl.efnetrbl.org', 'dyna.spamrats.com', 'all.s5h.net', 'b.barracudacentral.org', 'spam.spamrats.com'];
 const HOSTS_FILE = 'config/hosts.csv';
 const PROXIES_FILE = 'config/proxies.csv';
 
