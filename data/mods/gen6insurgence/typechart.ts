2cd8cbc8d663b4a18ebc481e275b033ed6bacf7d
vikrambarr <37145312+vikrambarr@users.noreply.github.com>
2024-12-06 00:54:10 +0000

    fix

--- data/mods/gen6insurgence/typechart.ts
+++ data/mods/gen6insurgence/typechart.ts
@@ -1,3 +1,4 @@
+const {Dex} = require('../../../sim/dex');
 export const TypeChart: import('../../../sim/dex-data').ModdedTypeDataTable = {
 	...Dex.deepClone(require('../gen9insurgence/typechart').ModTypeChart),
 };
--- data/mods/gen6uranium/typechart.ts
+++ data/mods/gen6uranium/typechart.ts
@@ -1,3 +1,4 @@
+const {Dex} = require('../../../sim/dex');
 export const TypeChart: import('../../../sim/dex-data').ModdedTypeDataTable = {
 	...Dex.deepClone(require('../gen9uranium/typechart').ModTypeChart),
 };
