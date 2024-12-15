e9ca84c34b6523958d94222c2dbaa7ee5250dc91
vikrambarr <37145312+vikrambarr@users.noreply.github.com>
2024-12-15 13:01:47 +0000

    see if anyone notices

--- data/random-battles/gen7infinitefusion/teams.ts
+++ data/random-battles/gen7infinitefusion/teams.ts
@@ -21,9 +21,6 @@ export class RandomGen7Teams extends RandomGen8Teams {
 			const curSpecies = curSet.species;
 			const curFusion = curSet.fusion;
 
-			// Level balance--calculate directly from stats rather than using some silly lookup table
-			const mbstmin = 1307;
-
 			const species_stats = this.dex.species.get(curSpecies).baseStats;
 			const fusion_stats = this.dex.species.get(curFusion).baseStats
 
@@ -36,33 +33,7 @@ export class RandomGen7Teams extends RandomGen8Teams {
 				spe: Math.floor((species_stats.spe * (1 / 3)) + (fusion_stats.spe * (2 / 3))),
 			};
 
-			// Modified base stat total assumes 31 IVs, 85 EVs in every stat
-			let mbst = (stats["hp"] * 2 + 31 + 21 + 100) + 10;
-			mbst += (stats["atk"] * 2 + 31 + 21 + 100) + 5;
-			mbst += (stats["def"] * 2 + 31 + 21 + 100) + 5;
-			mbst += (stats["spa"] * 2 + 31 + 21 + 100) + 5;
-			mbst += (stats["spd"] * 2 + 31 + 21 + 100) + 5;
-			mbst += (stats["spe"] * 2 + 31 + 21 + 100) + 5;
-
-			let level;
-			if (this.adjustLevel) {
-				level = this.adjustLevel;
-			} else {
-				level = Math.floor(100 * mbstmin / mbst); // Initial level guess will underestimate
-
-				while (level < 100) {
-					mbst = Math.floor((stats["hp"] * 2 + 31 + 21 + 100) * level / 100 + 10);
-					// Since damage is roughly proportional to level
-					mbst += Math.floor(((stats["atk"] * 2 + 31 + 21 + 100) * level / 100 + 5) * level / 100);
-					mbst += Math.floor((stats["def"] * 2 + 31 + 21 + 100) * level / 100 + 5);
-					mbst += Math.floor(((stats["spa"] * 2 + 31 + 21 + 100) * level / 100 + 5) * level / 100);
-					mbst += Math.floor((stats["spd"] * 2 + 31 + 21 + 100) * level / 100 + 5);
-					mbst += Math.floor((stats["spe"] * 2 + 31 + 21 + 100) * level / 100 + 5);
-
-					if (mbst >= mbstmin) break;
-					level++;
-				}
-			}
+			let level = Math.floor(((30 / Math.max(...Object.values(stats))) ** 0.2) * 100);
 
 			if (curSet.moves && curSet.ability && curSet.item) {
 				pokemon.push({
