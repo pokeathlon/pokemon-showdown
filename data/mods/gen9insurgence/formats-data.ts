d50bdd300a5e513eaf7fefdb43712bd196ac996b
vikrambarr <37145312+vikrambarr@users.noreply.github.com>
2024-12-18 10:52:38 +0000

    i hate hbf

---
+++ data/mods/gen9insurgence/formats-data.ts
@@ -0,0 +1,150 @@
+export const FormatsData: import('../../../sim/dex-species').ModdedSpeciesFormatsDataTable = {
+	scytherdelta: {
+		inherit: true,
+		natDexTier: "LC",
+	},
+	vulpixalola: {
+		inherit: true,
+		natDexTier: "LC",
+	},
+	diglett: {
+		inherit: true,
+		natDexTier: "LC",
+	},
+	gastly: {
+		inherit: true,
+		natDexTier: "LC",
+	},
+	tangela: {
+		inherit: true,
+		natDexTier: "LC",
+	},
+	scyther: {
+		inherit: true,
+		natDexTier: "LC",
+	},
+	porygon: {
+		inherit: true,
+		natDexTier: "LC",
+	},
+	aipom: {
+		inherit: true,
+		natDexTier: "LC",
+	},
+	yanma: {
+		inherit: true,
+		natDexTier: "LC",
+	},
+	murkrow: {
+		inherit: true,
+		natDexTier: "LC",
+	},
+	misdreavus: {
+		inherit: true,
+		natDexTier: "LC",
+	},
+	girafarig: {
+		inherit: true,
+		natDexTier: "LC",
+	},
+	dunsparce: {
+		inherit: true,
+		natDexTier: "LC",
+	},
+	gligar: {
+		inherit: true,
+		natDexTier: "LC",
+	},
+	qwilfishhisui: {
+		inherit: true,
+		natDexTier: "LC",
+	},
+	sneasel: {
+		inherit: true,
+		natDexTier: "LC",
+	},
+	sneaselhisui: {
+		inherit: true,
+		natDexTier: "LC",
+	},
+	corsolagalar: {
+		inherit: true,
+		natDexTier: "LC",
+	},
+	stantler: {
+		inherit: true,
+		natDexTier: "LC",
+	},
+	zigzagoon: {
+		inherit: true,
+		natDexTier: "LC",
+	},
+	meditite: {
+		inherit: true,
+		natDexTier: "LC",
+	},
+	clamperl: {
+		inherit: true,
+		natDexTier: "LC",
+	},
+	drifloon: {
+		inherit: true,
+		natDexTier: "LC",
+	},
+	snivy: {
+		inherit: true,
+		natDexTier: "LC",
+	},
+	woobat: {
+		inherit: true,
+		natDexTier: "LC",
+	},
+	basculin: {
+		inherit: true,
+		natDexTier: "LC",
+	},
+	basculinbluestriped: {
+		inherit: true,
+		natDexTier: "LC",
+	},
+	basculinwhitestriped: {
+		inherit: true,
+		natDexTier: "LC",
+	},
+	scraggy: {
+		inherit: true,
+		natDexTier: "LC",
+	},
+	rufflet: {
+		inherit: true,
+		natDexTier: "LC",
+	},
+	swirlix: {
+		inherit: true,
+		natDexTier: "LC",
+	},
+	cutiefly: {
+		inherit: true,
+		natDexTier: "LC",
+	},
+	typenull: {
+		inherit: true,
+		natDexTier: "LC",
+	},
+	poipole: {
+		inherit: true,
+		natDexTier: "LC",
+	},
+	duraludon: {
+		inherit: true,
+		natDexTier: "LC",
+	},
+	kubfu: {
+		inherit: true,
+		natDexTier: "LC",
+	},
+	flittle: {
+		inherit: true,
+		natDexTier: "LC",
+	},
+};
