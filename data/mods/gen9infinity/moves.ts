461706b4a2996dbf95816f65832b7c6a54dd2108
vikrambarr <37145312+vikrambarr@users.noreply.github.com>
2025-01-12 20:21:14 +0000

    ambush

--- data/mods/gen9infinity/moves.ts
+++ data/mods/gen9infinity/moves.ts
@@ -355,6 +355,12 @@ export const ModMoves: import('../../../sim/dex-moves').ModdedMoveDataTable = {
 		pp: 10,
 		priority: 3,
 		flags: {contact: 1, protect: 1, mirror: 1},
+		onTry(source) {
+			if (source.activeMoveActions > 1) {
+				this.hint("Ambush only works on your first turn out.");
+				return false;
+			}
+		},
 		secondary: {
 			chance: 100,
 			volatileStatus: 'flinch',
