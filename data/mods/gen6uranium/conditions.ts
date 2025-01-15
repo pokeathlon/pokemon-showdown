46157fd976f84665a9e36ae219c353c29d9f313d
vikrambarr <37145312+vikrambarr@users.noreply.github.com>
2025-01-15 13:40:15 +0000

    revert sleeptalk to gen 2 in uranium

--- data/mods/gen6uranium/conditions.ts
+++ data/mods/gen6uranium/conditions.ts
@@ -1,2 +1,38 @@
 const {Dex} = require('../../../sim/dex');
-export const Conditions: import('../../../sim/dex-conditions').ModdedConditionDataTable = Dex.deepClone(require('../gen9uranium/conditions').ModConditions);
+export const Conditions: import('../../../sim/dex-conditions').ModdedConditionDataTable = {
+	slp: {
+		name: 'slp',
+		effectType: 'Status',
+		onStart(target, source, sourceEffect) {
+			if (sourceEffect && sourceEffect.effectType === 'Move') {
+				this.add('-status', target, 'slp', '[from] move: ' + sourceEffect.name);
+			} else {
+				this.add('-status', target, 'slp');
+			}
+			// 1-6 turns
+			this.effectState.time = this.random(2, 8);
+
+			if (target.removeVolatile('nightmare')) {
+				this.add('-end', target, 'Nightmare', '[silent]');
+			}
+		},
+		onSwitchIn(target) {
+			this.effectState.time += this.effectState.skippedTime;
+			this.effectState.skippedTime = 0;
+		},
+		onBeforeMovePriority: 10,
+		onBeforeMove(pokemon, target, move) {
+			pokemon.statusState.time--;
+			if (pokemon.statusState.time <= 0) {
+				pokemon.cureStatus();
+				return;
+			}
+			this.add('cant', pokemon, 'slp');
+			if (move.sleepUsable) {
+				return;
+			}
+			return false;
+		},
+	},
+	...Dex.deepClone(require('../gen9uranium/conditions').ModConditions)
+};
--- data/mods/gen6uranium/moves.ts
+++ data/mods/gen6uranium/moves.ts
@@ -1,2 +1,45 @@
 const {Dex} = require('../../../sim/dex');
-export const Moves: import('../../../sim/dex-moves').ModdedMoveDataTable = Dex.deepClone(require('../gen9uranium/moves').ModMoves);
+export const Moves: import('../../../sim/dex-moves').ModdedMoveDataTable = {
+	rest: {
+		inherit: true,
+		onTry(pokemon) {
+			if (pokemon.hp < pokemon.maxhp) return;
+			this.add('-fail', pokemon);
+			return null;
+		},
+		onHit(target, source, move) {
+			if (target.status !== 'slp') {
+				if (!target.setStatus('slp', source, move)) return;
+			} else {
+				this.add('-status', target, 'slp', '[from] move: Rest');
+			}
+			target.statusState.time = 3;
+			target.statusState.startTime = 3;
+			target.statusState.source = target;
+			this.heal(target.maxhp);
+		},
+		secondary: null,
+	},
+	sleeptalk: {
+		inherit: true,
+		flags: {failencore: 1, nosleeptalk: 1, nosketch: 1},
+		onTryHit(pokemon) {
+			return !pokemon.volatiles['choicelock'] && !pokemon.volatiles['encore'];
+		},
+		onHit(pokemon) {
+			const moves = [];
+			for (const moveSlot of pokemon.moveSlots) {
+				const moveid = moveSlot.id;
+				const move = this.dex.moves.get(moveid);
+				if (moveid && !move.flags['nosleeptalk'] && !move.flags['charge']) {
+					moves.push(moveid);
+				}
+			}
+			let randomMove = '';
+			if (moves.length) randomMove = this.sample(moves);
+			if (!randomMove) return false;
+			this.actions.useMove(randomMove, pokemon);
+		},
+	},
+	...Dex.deepClone(require('../gen9uranium/moves').ModMoves)
+};
