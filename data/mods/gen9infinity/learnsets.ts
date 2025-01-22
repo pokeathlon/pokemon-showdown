410437a964cc6d9b0ca89113f800ba426dd3120f
vikrambarr <37145312+vikrambarr@users.noreply.github.com>
2025-01-22 13:55:46 +0000

    gen 9 infinity additions

--- data/mods/gen9infinity/learnsets.ts
+++ data/mods/gen9infinity/learnsets.ts
@@ -10483,4 +10483,19 @@ export const ModLearnsets: import('../../../sim/dex-species').ModdedLearnsetData
 		},
 	},
 };
+
+const gen6lsets = require('../gen6infinity/learnsets').Learnsets
+for (const learnset in gen6lsets) {
+	if (learnset in baseLearnsets && !(learnset in ModLearnsets)) {
+		if (!gen6lsets[learnset].learnset || !baseLearnsets[learnset].learnset) continue;
+		for (const move in gen6lsets[learnset].learnset) {
+			if (move in baseLearnsets[learnset].learnset) continue;
+			if (!(learnset in ModLearnsets)) ModLearnsets[learnset as keyof typeof ModLearnsets] = {inherit: true, learnset: {...baseLearnsets[learnset as keyof typeof baseLearnsets].learnset}};
+
+			// @ts-ignore unable to stop this somehow
+			ModLearnsets[learnset as IDEntry].learnset[move as IDEntry] = gen6lsets[learnset].learnset[move];
+		}
+	}
+}
+
 export const Learnsets: import('../../../sim/dex-species').ModdedLearnsetDataTable = Dex.deepClone(ModLearnsets);
