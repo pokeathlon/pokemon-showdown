87c525b3bcd3b91cccea9e077eae2685b7c1fea5
Dunscy <0acrespo0@gmail.com>
2025-03-15 13:03:05 +0000

    Psychic Fangs Infinity gen 6

--- data/mods/gen6infinity/moves.ts
+++ data/mods/gen6infinity/moves.ts
@@ -1,4 +1,8 @@
 const {Dex} = require('../../../sim/dex');
 export const Moves: import('../../../sim/dex-moves').ModdedMoveDataTable = {
 	...Dex.deepClone(require('../gen9infinity/moves').ModMoves),
+	psychicfangs: {
+		inherit: true,
+		gen: 6,
+	},
 };
