1de8879d0491617ad2eeebe6b9eee4ad2ee11061
vikrambarr <37145312+vikrambarr@users.noreply.github.com>
2025-04-10 20:34:30 +0100

    evasion abilities banned

--- config/alt-formats.ts
+++ config/alt-formats.ts
@@ -328,7 +328,7 @@ export const Formats: import('../sim/dex-formats').FormatList = [
 		name: "[Gen 9] Mariomon OU",
 
 		mod: 'gen9mariomon',
-		ruleset: ['Standard', 'Swagger Clause', 'Terastal Clause', 'Z-Move Clause', 'No Event Moves', '+LGPE'],
+		ruleset: ['Standard', 'Evasion Abilities Clause', 'Swagger Clause', 'Terastal Clause', 'Z-Move Clause', 'No Event Moves', '+LGPE'],
 		banlist: [
 			'AG', 'Uber',
 			'ability:arenatrap', 'ability:shadowtag',
