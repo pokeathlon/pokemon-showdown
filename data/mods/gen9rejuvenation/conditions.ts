66686bfaea781388c1d5b9eeba17c04465f74ebb
Dunscy <0acrespo0@gmail.com>
2025-05-03 22:15:29 +0100

    Shadow Sky

--- data/mods/gen9rejuvenation/conditions.ts
+++ data/mods/gen9rejuvenation/conditions.ts
@@ -81,5 +81,41 @@ export const ModConditions: import('../../../sim/dex-conditions').ModdedConditio
 			this.damage(pokemon.baseMaxhp / modifier);
 		},
 	},
+	shadowsky: {
+		name: 'Shadow Sky',
+		effectType: 'Weather',
+		duration: 5,
+		durationCallback(source, effect) {
+			if (source?.hasItem('amplifiedrock')) {
+				return 8;
+			}
+			return 5;
+		},
+		onFieldStart(field, source, effect) {
+			if (effect?.effectType === 'Ability') {
+				this.add('-weather', 'Shadow Sky', '[from] ability: ' + effect.name, `[of] ${source}`);
+			} else {
+				this.add('-weather', 'Shadow Sky');
+			}
+		},
+		onWeatherModifyDamage(damage, attacker, defender, move) {
+			if (defender.hasItem('utilityumbrella')) return;
+			if (move.type === 'Shadow') {
+				this.debug('Shadow Sky fire boost');
+				return this.chainModify(1.5);
+			}
+		},
+		onFieldResidualOrder: 1,
+		onFieldResidual() {
+			this.add('-weather', 'Shadow Sky', '[upkeep]');
+			if (this.field.isWeather('shadowsky')) this.eachEvent('Weather');
+		},
+		onWeather(target) {
+			this.damage(target.baseMaxhp / 16);
+		},
+		onFieldEnd() {
+			this.add('-weather', 'none');
+		},
+	},
 };
 export const Conditions: import('../../../sim/dex-conditions').ModdedConditionDataTable = Dex.deepClone(ModConditions);
