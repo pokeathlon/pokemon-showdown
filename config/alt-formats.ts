3ad90e60868025095ed5e44f1c10e7bfacd506a5
vikrambarr <37145312+vikrambarr@users.noreply.github.com>
2025-07-02 13:57:13 +0100

    update insurgence draft formats

--- config/alt-formats.ts
+++ config/alt-formats.ts
@@ -275,7 +275,18 @@ export const Formats: import('../sim/dex-formats').FormatList = [
 		],
 	},
 	{
-		name: "[Gen 9] Ins NatDex Draft",
+		name: "[Gen 9] Ins NDL Draft",
+		searchShow: false,
+
+		mod: 'gen9insurgence',
+		ruleset: ['Standard NatDex', 'Max Level = 120', 'Default Level = 120', 'Overflow Stat Mod', 'Species Reveal Clause', '!Species Clause'],
+		banlist: [
+			'move:hiddenpower', 'move:lastrespects', 'move:shedtail', 'move:revivalblessing', 'move:nanorepair', 'move:livewire', 'move:permafrost', 'move:achillesheel', 'ability:powerconstruct', 'ability:winterjoy', 
+			'Emolga-Delta+Blaze Boost', 'Blastoise-Mega+Shell Smash', 'Regurgitation+Rock Blast', 'Parental Bond+Seismic Toss', 'Parental Bond+Night Shade', 
+		],
+	},
+	{
+		name: "[Gen 9] Ins HBF Draft",
 		searchShow: false,
 
 		mod: 'gen9insurgence',
@@ -283,7 +294,7 @@ export const Formats: import('../sim/dex-formats').FormatList = [
 		banlist: ['move:hiddenpower'],
 	},
 	{
-		name: "[Gen 9] Ins NatDex MM Draft",
+		name: "[Gen 9] Ins HBF MM Draft",
 		searchShow: false,
 		mod: 'gen9insurgence',
 		ruleset: ['Standard NatDex', 'Tera Type Preview', 'Swagger Clause', 'Max Level = 120', 'Default Level = 120', 'Overflow Stat Mod', 'Species Reveal Clause', '!Species Clause', 'Multiple Mega'],
