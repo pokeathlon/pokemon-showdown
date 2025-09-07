131d2f39a89a4018363d249fd76df6fb290b78d8
BeeruhBeement <58148912+BeeruhBeement@users.noreply.github.com>
2025-09-07 20:37:58 +0200

    harptera chaos ou

--- data/mods/gen9chaos/formats-data.ts
+++ data/mods/gen9chaos/formats-data.ts
@@ -287,6 +287,10 @@ export const FormatsData: import('../../../sim/dex-species').ModdedSpeciesFormat
 		inherit: true,
 		natDexTier: "OU",
 	},
+	harptera: {
+		inherit: true,
+		natDexTier: 'OU',
+	},
 	oblivicornmega: {
 		inherit: true,
 		natDexTier: "UUBL",
@@ -375,6 +379,10 @@ export const FormatsData: import('../../../sim/dex-species').ModdedSpeciesFormat
 		inherit: true,
 		natDexTier: "UUBL",
 	},
+	madpiano: {
+		inherit: true,
+		natDexTier: "UUBL",
+	},
 	walkingwake: {
 		inherit: true,
 		natDexTier: "UU",
@@ -384,10 +392,6 @@ export const FormatsData: import('../../../sim/dex-species').ModdedSpeciesFormat
 		natDexTier: "UU",
 	},
 	manacra: {
-		inherit: true,
-		natDexTier: "UU",
-	},
-	madpiano: {
 		inherit: true,
 		natDexTier: "UU",
 	},
@@ -924,10 +928,6 @@ export const FormatsData: import('../../../sim/dex-species').ModdedSpeciesFormat
 		natDexTier: 'RU',
 	},
 	haagrossmel: {
-		inherit: true,
-		natDexTier: 'RU',
-	},
-	harptera: {
 		inherit: true,
 		natDexTier: 'RU',
 	},
