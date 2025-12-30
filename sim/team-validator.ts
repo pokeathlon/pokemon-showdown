d8bd6c73b8e2de8824b3880ab9030748d49a0276
BeeruhBeement <58148912+BeeruhBeement@users.noreply.github.com>
2025-12-30 21:33:31 +0100

    no hidden power fangame types

--- sim/team-validator.ts
+++ sim/team-validator.ts
@@ -686,7 +686,7 @@ export class TeamValidator {
 		}
 		if (set.hpType) {
 			const type = dex.types.get(set.hpType);
-			if (!type.exists || ['normal', 'fairy', 'stellar'].includes(type.id)) {
+			if (!type.exists || ['normal', 'fairy', 'stellar', 'nuclear', 'crystal', 'cosmic'].includes(type.id)) {
 				problems.push(`${name}'s Hidden Power type (${set.hpType}) is invalid.`);
 			} else {
 				set.hpType = type.name;
