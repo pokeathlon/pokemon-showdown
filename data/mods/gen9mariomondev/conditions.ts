4cdbd401803d05ff31f31bae460924af4a819926
Dunscy <87420792+Dunscy@users.noreply.github.com>
2026-01-12 16:51:50 +0000

    Mariomon Dev mod

---
+++ data/mods/gen9mariomondev/conditions.ts
@@ -0,0 +1,192 @@
+export const Conditions: import('../../../sim/dex-conditions').ModdedConditionDataTable = {
+	frz: {
+		inherit: true,
+		onStart(target, source, sourceEffect) {
+			if (sourceEffect && sourceEffect.effectType === 'Ability') {
+				this.add('-status', target, 'frz', '[from] ability: ' + sourceEffect.name, `[of] ${source}`);
+			} else {
+				this.add('-status', target, 'frz');
+			}
+			if (target.species.name === 'Shaymin-Sky' && target.baseSpecies.baseSpecies === 'Shaymin') {
+				target.formeChange('Shaymin', this.effect, true);
+			}
+			this.add('-message', 'Freeze is replaced with Frostbite in Super Mariomon (special damage is halved, and 1/16 HP is lost each turn).');
+		},
+		// Damage reduction is handled directly in the sim/battle.js damage function
+		onResidualOrder: 10,
+		onResidual(pokemon) {
+			this.damage(pokemon.baseMaxhp / 16);
+		},
+		onBeforeMove(pokemon, target, move) {},
+	},
+	raindance: {
+		inherit: true,
+		onFieldStart(field, source, effect) {
+			if (effect?.effectType === 'Ability') {
+				if (this.gen <= 5) this.effectState.duration = 0;
+				this.add('-weather', 'RainDance', '[from] ability: ' + effect.name, `[of] ${source}`);
+			} else {
+				this.add('-weather', 'RainDance');
+			}
+			for (const target of this.getAllPokemon()) {
+				for (const move of target.moveSlots) {
+					if (['hammerthrow', 'boomerang', 'fireball', 'icetoss'].includes(move.id)) {
+						move.id = 'boomerang' as ID;
+						move.move = 'Boomerang';
+					}
+				}
+			}
+		},
+		onFieldEnd() {
+			for (const target of this.getAllPokemon()) {
+				for (const move of target.moveSlots) {
+					if (['hammerthrow', 'boomerang', 'fireball', 'icetoss'].includes(move.id)) {
+						move.id = 'hammerthrow' as ID;
+						move.move = 'Hammer Throw';
+					}
+				}
+			}
+			this.add('-weather', 'none');
+		},
+	},
+	primordialsea: {
+		inherit: true,
+		onFieldStart(field, source, effect) {
+			this.add('-weather', 'PrimordialSea', '[from] ability: ' + effect.name, `[of] ${source}`);
+			for (const target of this.getAllPokemon()) {
+				for (const move of target.moveSlots) {
+					if (['hammerthrow', 'boomerang', 'fireball', 'icetoss'].includes(move.id)) {
+						move.id = 'boomerang' as ID;
+						move.move = 'Boomerang';
+					}
+				}
+			}
+		},
+		onFieldEnd() {
+			for (const target of this.getAllPokemon()) {
+				for (const move of target.moveSlots) {
+					if (['hammerthrow', 'boomerang', 'fireball', 'icetoss'].includes(move.id)) {
+						move.id = 'hammerthrow' as ID;
+						move.move = 'Hammer Throw';
+					}
+				}
+			}
+			this.add('-weather', 'none');
+		},
+	},
+	sunnyday: {
+		inherit: true,
+		onFieldStart(battle, source, effect) {
+			if (effect?.effectType === 'Ability') {
+				if (this.gen <= 5) this.effectState.duration = 0;
+				this.add('-weather', 'SunnyDay', '[from] ability: ' + effect.name, `[of] ${source}`);
+			} else {
+				this.add('-weather', 'SunnyDay');
+			}
+			for (const target of this.getAllPokemon()) {
+				for (const move of target.moveSlots) {
+					if (['hammerthrow', 'boomerang', 'fireball', 'icetoss'].includes(move.id)) {
+						move.id = 'fireball' as ID;
+						move.move = 'Fire Ball';
+					}
+				}
+			}
+		},
+		onFieldEnd() {
+			for (const target of this.getAllPokemon()) {
+				for (const move of target.moveSlots) {
+					if (['hammerthrow', 'boomerang', 'fireball', 'icetoss'].includes(move.id)) {
+						move.id = 'hammerthrow' as ID;
+						move.move = 'Hammer Throw';
+					}
+				}
+			}
+			this.add('-weather', 'none');
+		},
+	},
+	desolateland: {
+		inherit: true,
+		onFieldStart(field, source, effect) {
+			this.add('-weather', 'DesolateLand', '[from] ability: ' + effect.name, `[of] ${source}`);
+			for (const target of this.getAllPokemon()) {
+				for (const move of target.moveSlots) {
+					if (['hammerthrow', 'boomerang', 'fireball', 'icetoss'].includes(move.id)) {
+						move.id = 'fireball' as ID;
+						move.move = 'Fire Ball';
+					}
+				}
+			}
+		},
+		onFieldEnd() {
+			for (const target of this.getAllPokemon()) {
+				for (const move of target.moveSlots) {
+					if (['hammerthrow', 'boomerang', 'fireball', 'icetoss'].includes(move.id)) {
+						move.id = 'hammerthrow' as ID;
+						move.move = 'Hammer Throw';
+					}
+				}
+			}
+			this.add('-weather', 'none');
+		},
+	},
+	hail: {
+		inherit: true,
+		onFieldStart(field, source, effect) {
+			if (effect?.effectType === 'Ability') {
+				if (this.gen <= 5) this.effectState.duration = 0;
+				this.add('-weather', 'Hail', '[from] ability: ' + effect.name, `[of] ${source}`);
+			} else {
+				this.add('-weather', 'Hail');
+			}
+			for (const target of this.getAllPokemon()) {
+				for (const move of target.moveSlots) {
+					if (['hammerthrow', 'boomerang', 'fireball', 'icetoss'].includes(move.id)) {
+						move.id = 'icetoss' as ID;
+						move.move = 'Ice Toss';
+					}
+				}
+			}
+		},
+		onFieldEnd() {
+			for (const target of this.getAllPokemon()) {
+				for (const move of target.moveSlots) {
+					if (['hammerthrow', 'boomerang', 'fireball', 'icetoss'].includes(move.id)) {
+						move.id = 'hammerthrow' as ID;
+						move.move = 'Hammer Throw';
+					}
+				}
+			}
+			this.add('-weather', 'none');
+		},
+	},
+	snowscape: {
+		inherit: true,
+		onFieldStart(field, source, effect) {
+			if (effect?.effectType === 'Ability') {
+				if (this.gen <= 5) this.effectState.duration = 0;
+				this.add('-weather', 'Snowscape', '[from] ability: ' + effect.name, `[of] ${source}`);
+			} else {
+				this.add('-weather', 'Snowscape');
+			}
+			for (const target of this.getAllPokemon()) {
+				for (const move of target.moveSlots) {
+					if (['hammerthrow', 'boomerang', 'fireball', 'icetoss'].includes(move.id)) {
+						move.id = 'icetoss' as ID;
+						move.move = 'Ice Toss';
+					}
+				}
+			}
+		},
+		onFieldEnd() {
+			for (const target of this.getAllPokemon()) {
+				for (const move of target.moveSlots) {
+					if (['hammerthrow', 'boomerang', 'fireball', 'icetoss'].includes(move.id)) {
+						move.id = 'hammerthrow' as ID;
+						move.move = 'Hammer Throw';
+					}
+				}
+			}
+			this.add('-weather', 'none');
+		},
+	},
+};
---
+++ data/mods/gen9mariomondev/moves.ts
@@ -0,0 +1,104 @@
+import { Moves as Base } from '../../moves';
+
+export const Moves: import('../../../sim/dex-moves').ModdedMoveDataTable = {
+	// Modded
+	darkvoid: {
+		inherit: true,
+		accuracy: 80,
+		onTry(source, target, move) {
+			if (source.species.name === 'Antasma' || move.hasBounced) {
+				return;
+			}
+			this.add('-fail', source, 'move: Dark Void');
+			this.hint("Only a Pokemon whose form is Antasma can use this move.");
+			return null;
+		},
+	},
+	sizzlyslide: {
+		inherit: true,
+		isNonstandard: null,
+	},
+
+	// Additions
+	hammerthrow: {
+		num: 0,
+		accuracy: 85,
+		basePower: 25,
+		category: "Physical",
+		name: "Hammer Throw",
+		shortDesc: "Hits 2-5 times. Changes based on the weather.",
+		multihit: [2, 5],
+		pp: 20,
+		priority: 0,
+		flags: {protect: 1, mirror: 1},
+		secondary: null,
+		target: "normal",
+		type: "Normal",
+	},
+	fireball: {
+		num: 0,
+		accuracy: 85,
+		basePower: 25,
+		category: "Special",
+		name: "Fire Ball",
+		shortDesc: "Hits 3 times. 15% chance to burn.",
+		multihit: 3,
+		secondary: {
+			chance: 15,
+			status: 'brn',
+		},
+		pp: 20,
+		priority: 0,
+		flags: {protect: 1, mirror: 1, nosketch: 1},
+		isNonstandard: "Unobtainable",
+		target: "normal",
+		type: "Fire",
+	},
+	boomerang: {
+		num: 0,
+		accuracy: 90,
+		basePower: 45,
+		category: "Physical",
+		name: "Boomerang",
+		shortDesc: "Hits 2 times. 15% chance to flinch.",
+		multihit: 2,
+		secondary: {
+			chance: 15,
+			volatileStatus: 'flinch'
+		},
+		pp: 20,
+		priority: 0,
+		flags: {protect: 1, mirror: 1, nosketch: 1},
+		isNonstandard: "Unobtainable",
+		target: "normal",
+		type: "Flying",
+	},
+	icetoss: {
+		num: 0,
+		accuracy: 85,
+		basePower: 25,
+		category: "Special",
+		name: "Ice Toss",
+		shortDesc: "Hits 3 times. 15% chance to frostbite.",
+		multihit: 3,
+		secondary: {
+			chance: 15,
+			status: 'frz',
+		},
+		pp: 20,
+		priority: 0,
+		flags: {protect: 1, mirror: 1, nosketch: 1},
+		isNonstandard: "Unobtainable",
+		target: "normal",
+		type: "Ice",
+	},
+};
+
+for (const key in Base) {
+	const id = key as keyof typeof Base;
+	if (Moves[id]) continue;
+
+	if (Base[id].isNonstandard && ["Past", "Unobtainable"].includes(Base[id].isNonstandard)) {
+		Moves[id] = {inherit: true, isNonstandard: null};
+	}
+}
