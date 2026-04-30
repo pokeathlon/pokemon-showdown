63d3022d8098f1ab90159277ddd38307c8dc1d4e
Dunscy <87420792+Dunscy@users.noreply.github.com>
2026-04-30 19:19:27 +0100

    Floette-E Mega

--- data/mods/gen9chaos/pokedex.ts
+++ data/mods/gen9chaos/pokedex.ts
@@ -105,6 +105,10 @@ export const Pokedex: ModdedSpeciesDataTable = {
 		inherit: true,
 		evos: ["Staruhz"],
 	},
+	floetteeternal: {
+		inherit: true,
+		baseSpecies: undefined,
+	},
 
 	// Additions
 	ashenash: {
