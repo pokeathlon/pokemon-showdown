c8c7265ad3b71d25551d29c05073b7a81d9d6827
Dunscy <87420792+Dunscy@users.noreply.github.com>
2026-06-02 10:07:15 +0200

    legalize verv

--- data/mods/gen9pokeathlon/formats-data.ts
+++ data/mods/gen9pokeathlon/formats-data.ts
@@ -1106,6 +1106,12 @@ export const FormatsData: import('../../../sim/dex-species').ModdedSpeciesFormat
 		natDexTier: "OU",
 		isNonstandard: null,
 	},
+	vervestra: {
+		inherit: true,
+		tier: "OU",
+		natDexTier: "OU",
+		isNonstandard: null,
+	},
 };
 
 for (const pokemon in Base) {
